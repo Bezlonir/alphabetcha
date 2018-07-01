e0e3af8c3bf1621c62b9e6213900f8e2f25b4079
Bezlonir <michael_bragg99@hotmail.com>
2018-07-01 12:21:06 -0600

    Change layout.

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,16 +4,10 @@ var blet = document.querySelector('#b-letter');
 
 checkImages.forEach(function(alphaLabel){
   alphaLabel.addEventListener('click', function(){
-    console.dir(this);
     var nv = this.attributes.for.nodeValue;
-    console.log(nv);
     var radThis = document.querySelector(`#${nv}`);
     var bigLetter = document.querySelector('#big-letter');
     var letDex = radThis.attributes.value.value;
-    console.log(radThis.attributes);
-    console.log(letDex);
-    console.log(`./resources/images/${letDex}.jpg`);
     bigLetter.style.backgroundImage = `url('./resources/images/${letDex}.jpg')`;
-    console.log(bigLetter.style.backgroundImage);
   });
 });
